b043e2f6b2ce470a631161515d225ae59a6fc111
Deepak Jayaram <dpk@Deepaks-MacBook-Pro.local>
2018-02-11 22:33:03 -0500

    Initial Commit - adding a basic object generator

---
+++ index.js
@@ -0,0 +1,83 @@
+const faker = require("faker");
+const { vehicles, vehicleCondition } = require("./vehicleData");
+const {
+  date: fkDate,
+  company: fkCompany,
+  name: fkName,
+  address: fkAddress,
+  internet: fkInternet,
+  commerce: fkCommerce,
+  random: fkRandom
+} = faker;
+
+const NUM_OBJ = 5;
+const NUM_FEATURED_INV = 4;
+
+const generateBasicInfo = () => {
+  const firstName = fkName.firstName();
+  const lastName = fkName.lastName();
+  const email = fkInternet.exampleEmail(firstName, lastName);
+  return {
+    firstName,
+    lastName,
+    email
+  };
+};
+
+const generateDealershipInfo = () => {
+  const dealershipName = fkCompany.companyName(0);
+  const state = fkAddress.state();
+  return {
+    dealershipName,
+    state
+  };
+};
+
+const determineMileage = year => {
+  const currentYear = new Date().getFullYear();
+  const prevYear = Number(year);
+  return (
+    (currentYear - prevYear) * fkRandom.number({ min: 10000, max: 120000 })
+  );
+};
+
+const generateRandomVehicleData = () => {
+  const randomVehicleObj = fkRandom.arrayElement(vehicles);
+  const make = randomVehicleObj.make;
+  const model = fkRandom.arrayElement(randomVehicleObj.model);
+  const trim = fkRandom.arrayElement(randomVehicleObj.trim);
+  const color = fkCommerce.color();
+  const condition = fkRandom.arrayElement(vehicleCondition);
+  const year = String(
+    condition === "new"
+      ? new Date().getFullYear()
+      : new Date(fkDate.past(10)).getFullYear()
+  );
+  const vehicleObj = { make, model, trim, condition, color, year };
+  const mileage = determineMileage(year);
+  return condition === "new" ? vehicleObj : { ...vehicleObj, mileage };
+};
+
+const generateInventoryInfo = len => {
+  const featuredInventory = [];
+  for (let i = 0; i < len; i++) {
+    featuredInventory.push({
+      ...generateRandomVehicleData()
+    });
+  }
+  return { featuredInventory };
+};
+
+generateDealerObj = (len = 10, inv_num = 1) => {
+  const dealers = [];
+  for (let i = 0; i < len; i++) {
+    dealers.push({
+      ...generateBasicInfo(),
+      ...generateDealershipInfo(),
+      ...generateInventoryInfo(inv_num)
+    });
+  }
+  return JSON.stringify({ dealers });
+};
+
+console.log(generateDealerObj(NUM_OBJ, NUM_FEATURED_INV));
---
+++ vehicleData.js
@@ -0,0 +1,27 @@
+const vehicleData = {
+  vehicles: [
+    {
+      make: "Kia",
+      model: ["Optima", "Soul", "Sportage", "Rio", "Forte"],
+      trim: ["LX", "VX", "ZX", "Sport", "Hybrid"]
+    },
+    {
+      make: "Volvo",
+      model: ["XC90", "XC70", "XC60", "S60", "S80"],
+      trim: ["T5 AWD Dynamic", "T5 FWD Dynamic"]
+    },
+    {
+      make: "Mazda",
+      model: ["CX-5", "CX-3", "MX-5 Miata", "CX-9", "3", "6"],
+      trim: ["Sport", "Touring", "Grand Touring"]
+    },
+    {
+      make: "Mercedes",
+      model: ["C-Class", "E-Class", "S-Class", "CLA", "GLA", "GLE"],
+      trim: ["4Matic", "AMG", "Hybrid"]
+    }
+  ],
+  vehicleCondition: ["New", "Preowned"]
+};
+
+module.exports = vehicleData;
